182291905da9c91af2e455fff416b7d20aa4a078
Mohammed Atef <matef@e1r9p15.1337.ma>
2023-01-27 22:58:45 +0100

    to vm

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+
+
+if [ -f /var/www/html/wp.config.php ]; then
+
 cd /var/www/html
-
-
 wget http://wordpress.org/latest.tar.gz
 tar xfz latest.tar.gz
 cp -Rf wordpress/* /var/www/html/
@@ -16,16 +18,13 @@ sed -i "32s/localhost/$DB_HOST/" wp-config.php
 
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
-
 
 wp core install --url=$DOMAIN_NAME/ --title=$WP_SITE_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL --allow-root
 wp user create $WP_AUTHOR $WP_AUTHOR_EMAIL --role=author --user_pass=$WP_AUTHOR_PWD --allow-root
 
+fi
 
-mkdir /run/php
 /usr/sbin/php-fpm7.3 -F
-
 exec "$@"
